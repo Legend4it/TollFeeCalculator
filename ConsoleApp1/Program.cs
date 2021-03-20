f9140de4336b72a2384c15342780dae80e0cde18
SENNA0 <ali.abdulhussein@flexlink.com>
2021-03-20 20:28:20 +0100

    add new test case TollFreeVehicle return 0, add program implementation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using TollFeeCalculator;
 
 namespace ConsoleApp1
 {
@@ -7,6 +9,40 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            var vehicle1 = new Car("ABC123");
+            GetFee(vehicle1, GetDatesData());
+
+            var vehicle2 = new Motorbike("ABC123");
+            GetFee(vehicle2, GetDatesData());
+            Console.ReadKey();
+
         }
+        public static void GetFee(Vehicle vehicle, List<DateTime> dates)
+        {
+            var tc = new TollCalculator();
+            Console.WriteLine($"Total Fee:{tc.GetTollFee(vehicle, dates.ToArray())}, Vehicle Typ:{vehicle.GetVehicleType()}");
+
+        }
+        public static List<DateTime> GetDatesData()
+        {
+            return new List<DateTime>() {
+            new DateTime(2021, 03, 18, 06, 20, 00),
+            new DateTime(2021, 03, 18, 06, 35, 00),
+            new DateTime(2021, 03, 18, 06, 44, 00),
+            new DateTime(2021, 03, 18, 06, 45, 00),
+            new DateTime(2021, 03, 18, 06, 55, 00),
+            new DateTime(2021, 03, 18, 07, 10, 00),
+            new DateTime(2021, 03, 18, 07, 55, 00),
+            new DateTime(2021, 03, 18, 08, 20, 00),
+            new DateTime(2021, 03, 18, 08, 55, 00),
+            new DateTime(2021, 03, 18, 14, 50, 00),
+            new DateTime(2021, 03, 18, 15, 00, 00),
+            new DateTime(2021, 03, 18, 15, 20, 00),
+            new DateTime(2021, 03, 18, 16, 55, 00),
+            new DateTime(2021, 03, 18, 17, 00, 00),
+            new DateTime(2021, 03, 18, 18, 55, 00)
+        };
+        }
+
     }
 }
--- NUnitTestProject/UnitTest.cs
+++ NUnitTestProject/UnitTest.cs
@@ -8,10 +8,14 @@ namespace NUnitTestProject
 {
     public class Tests
     {
+        private List<DateTime> _dates;
         [SetUp]
         public void Setup()
         {
+            _dates = GetDates();
+
         }
+
 
         [Test]
         public void IsTollFreeVehicle()
@@ -39,21 +43,41 @@ namespace NUnitTestProject
         [Test]
         public void GetMaxFeePerDagIsSuccess()
         {
-            var dates = new List<DateTime>(); //date and time of all passes on one day
-            dates.Add(new DateTime(2021, 03, 18, 10, 10, 00));
-            dates.Add(new DateTime(2021, 03, 18, 10, 15, 00));
-            dates.Add(new DateTime(2021, 03, 18, 11, 00, 00));
-            dates.Add(new DateTime(2021, 03, 18, 11, 30, 00));
-            dates.Add(new DateTime(2021, 03, 18, 12, 00, 00));
-            dates.Add(new DateTime(2021, 03, 18, 13, 00, 00));
-            dates.Add(new DateTime(2021, 03, 18, 14, 00, 00));
-            dates.Add(new DateTime(2021, 03, 18, 14, 30, 00));
             var vehicle = new Car("ABC123");
             var tc = new TollCalculator();
-            var actual = tc.GetTollFee(vehicle, dates.ToArray());
+            var actual = tc.GetTollFee(vehicle, _dates.ToArray());
 
             Assert.IsTrue(actual <= 60);
         }
+        [Test]
+        public void GetTollFreeVehicleIsSuccess()
+        {
+            var vehicle = new Motorbike("ABC123");
+            var tc = new TollCalculator();
+            var actual = tc.GetTollFee(vehicle, _dates.ToArray());
 
+            Assert.IsTrue(actual == 0);
+        }
+
+        private List<DateTime> GetDates()
+        {
+            return new List<DateTime>() {
+            new DateTime(2021, 03, 18, 06, 20, 00),
+            new DateTime(2021, 03, 18, 06, 35, 00),
+            new DateTime(2021, 03, 18, 06, 44, 00),
+            new DateTime(2021, 03, 18, 06, 45, 00),
+            new DateTime(2021, 03, 18, 06, 55, 00),
+            new DateTime(2021, 03, 18, 07, 10, 00),
+            new DateTime(2021, 03, 18, 07, 55, 00),
+            new DateTime(2021, 03, 18, 08, 20, 00),
+            new DateTime(2021, 03, 18, 08, 55, 00),
+            new DateTime(2021, 03, 18, 14, 50, 00),
+            new DateTime(2021, 03, 18, 15, 00, 00),
+            new DateTime(2021, 03, 18, 15, 20, 00),
+            new DateTime(2021, 03, 18, 16, 55, 00),
+            new DateTime(2021, 03, 18, 17, 00, 00),
+            new DateTime(2021, 03, 18, 18, 55, 00)
+        };
+        }
     }
 }
